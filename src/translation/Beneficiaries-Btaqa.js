b3f138a11b1511ddca81b3a7350cbaeb88ca5d4f
Nourhan102 <nourhan@gmail.com>
2026-08-15 11:33:15 +0300

    تحديث المشروع

---
+++ src/translation/Beneficiaries-Btaqa.js
@@ -0,0 +1,18 @@
+ 
+ 
+ Object.assign(translations.ar, {
+
+title:"  المستفيدون من بطاقة المحارب العربى القديم  ",
+hedeer:"  الإتحاد العربى للمحاربين القدماء وضحايا الحرب يضم جميع أعضاء الجمعيات والإدارات والهيئات والشعبة أعضاء الإتحاد العربى من المحاربين المقاتلين المصابين القدماء الذى قدموا أرواحهم فداءٌ وتضحية لوطنهم العظيم وأسرهم ( الأب – الأم – الزوجة – الأولاد )  .  "
+
+ });
+
+
+
+
+   Object.assign(translations.en,{
+
+title:"  Beneficiaries of the Arab Veteran Card  ",
+hedeer:"   The Arab Federation of Veterans and War Victims includes all members of associations, departments, bodies, and divisions affiliated with the Arab Federation—comprising combat veterans and injured veterans who sacrificed their lives for their great homeland—as well as their families (father, mother, wife, and children)  .   "
+
+});
---
+++ src/translation/Ruls-Btaqa.js
@@ -0,0 +1,35 @@
+ 
+ 
+ Object.assign(translations.ar, {
+
+title_one: "  تعليمات بشأن إستخرج بطاقة المحارب العربى القديم  . ",
+title_two: "    تعليمات عامة :-  ",
+hedeer: "  فى إطار تفعيل الإستراتيجية العربية الجديدة المقترحة (2025م-2029) والتى تحتوى أحد محاورها الرئيسية تعظيم و تفعيل دور المحارب العربى القديم من خلال تقديم خدمات ومميزات للمحارب العربى القديم .  ",
+1:"   تعتبر البطاقة غير سارية المفعول بعد إنتهاء مدة عملها .  ",
+2:"  فى حالة تلف / فقد البطاقة يتم إبلاغ أمانة الإتحاد ورئيس مدير الجمعية التابعة له وإتخاذ كافة الإجراءات المتبعه فى هذا الشأن  .  ",
+3:"   قيام الجمعيات / المنظمات بالتواصل مع الأمانة بتحديث الأعداد والأسماء والبيانات للمحاربين والتواصل عن طريق الأيميل   .  ",
+4:"  البطاقات الشرفية السابقة التى تم إصدارها من الإتحاد والأعضاء أثناء عقد الدورات السابقة والحالية لا يعتد العمل بها وغير صالحة لتقديم خدمات للمحارب العربى   .  ",
+5:"   يتم إرسال نسخة من التعليمات إلى الجهات التى تم التنسيق معها  .  ",
+6:"  صلاحية البطاقة لمدة 5 سنوات حتى نهاية عام 2029 م  .  ",
+last:"   تأمل الأمانة العامة للإتحاد العربى للمحاربين القدماء وضحايا الحرب أن يتقدم أكبر عدد من أعضاء الجمعيات والمنظمات لإستخراج بطاقة المحارب العربى القديم الجديده فى التوقيت المحدد حتى يستفيد أكبر قدر من المحاربين القدماء العربى والذى قدموا أغلى ما يملكون   .  "
+
+
+ });
+
+
+
+
+   Object.assign(translations.en,{
+
+title_one: "   Instructions for Issuing the Ancient Arab Warrior Card .  ",
+title_two: "    General Guidelines :- ",
+hedeer:"   Within the framework of activating the proposed new Arab strategy (2025–2029), which includes as one of its main pillars the enhancement and activation of the role of veterans through providing services and benefits to them  . ",
+1:"   The card is considered invalid upon the expiration of its validity period  .  ",
+2:"   In the event of damage to or loss of the card, the Secretariat of the Federation and the President/Manager of the affiliated association must be notified, and all applicable procedures in this regard must be taken  .  ",
+3:"  Associations and organizations shall communicate with the Secretariat to update the numbers, names, and data of veterans via email  .  ",
+4:"  Previous honorary cards issued by the Federation and its members during past and current sessions are no longer recognized and are invalid for providing services to Arab veterans  .  ",
+5:"   A copy of these instructions shall be sent to the entities with which coordination has been established  .   ",
+6:"   The card is valid for a period of 5 years, ending at the close of the year 2029  .  ",
+last:"   The General Secretariat of the Arab Federation of Veterans and War Victims hopes that as many members of the associations and organizations as possible will apply to issue the new Arab Veteran Card within the specified time, so that the maximum number of Arab veterans who have sacrificed their most valuable possessions can benefit from it  .   "
+
+});
